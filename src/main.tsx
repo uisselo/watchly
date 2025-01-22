1a6a6e84fc1b608b838b08dd4845101ef08ddf6d
uisselo <louissemevale@gmail.com>
2025-01-22 15:26:55 +0800

    feat(main.tsx): set up react-query, import animate.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider } from "react-router";
 import "@fontsource-variable/quicksand";
 import "./index.css";
+import "animate.css";
 import router from "./router";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
