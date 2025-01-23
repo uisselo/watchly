b8b6bd68b1f0ab4784f2c7d546c90a52d4e47a55
uisselo <louissemevale@gmail.com>
2025-01-23 17:31:37 +0800

    feat(utils/functions.ts): create formatDate function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,4 +1,5 @@
 import clsx, { type ClassValue } from "clsx";
+import moment, { type Moment } from "moment";
 import { twMerge } from "tailwind-merge";
 
 /**
@@ -14,4 +15,15 @@ function cn(...classes: ClassValue[]) {
   return twMerge(clsx(...classes));
 }
 
-export { cn };
+/**
+ * Formats a given date into a specific string pattern.
+ *
+ * @param date - The date to be formatted. Can be a string, Moment object, or Date object.
+ * @param pattern - The format string specifying the desired date format.
+ *                  Defaults to "MMMM DD, YYYY" if not provided.
+ */
+function formatDate(date: string | Moment | Date, pattern?: string) {
+  return moment(date).format(pattern || "MMMM DD, YYYY");
+}
+
+export { cn, formatDate };
