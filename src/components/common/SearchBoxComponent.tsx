5ef85a306fd73db38f61b4519266a2bb67ac8cf2
uisselo <louissemevale@gmail.com>
2025-01-31 18:45:07 +0800

    feat(SearchBoxComponent): add default icon and jsdoc

--- src/components/common/SearchBoxComponent.tsx
+++ src/components/common/SearchBoxComponent.tsx
@@ -4,10 +4,22 @@ import {
   useState,
 } from "react";
 import { Input, type InputProps } from "@headlessui/react";
-import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import { cn } from "@Utilities";
 import ButtonIconComponent from "./ButtonIconComponent";
 
+/**
+ * A search box component with an input field and optional search/clear icons.
+ *
+ * @param children - Optional child elements to be displayed inside the search box.
+ * @param className - Additional class names for styling the search box.
+ * @param inputProps - Additional props passed to the `Input` component.
+ *
+ * @returns A styled search input field with optional icons and focus effect.
+ */
 function SearchBoxComponent(props: PropsWithChildren<InputProps>) {
   const { children, className, ...inputProps } = props;
 
@@ -19,21 +31,22 @@ function SearchBoxComponent(props: PropsWithChildren<InputProps>) {
   return (
     <div
       className={cn(
-        "custom-inner-shadow__top flex gap-1 relative px-3 py-2 bg-gray-600 md:px-4 md:py-3 rounded-[28px]",
+        "custom-inner-shadow__top grow flex items-center gap-1 relative px-3 py-2 bg-gray-600 md:px-4 md:py-3 rounded-[28px]",
         className,
       )}
     >
       <Input
-        className="w-full bg-transparent focus:outline-none"
+        className="w-full bg-transparent focus:outline-none grow"
         placeholder="Search"
         {...{ onFocus, onBlur }}
         {...inputProps}
       />
-
+      {children || (
+        <MagnifyingGlassIcon className="stroke-gray-400 stroke-1.5 size-4 md:size-5" />
+      )}
       {focused && (
         <div className="absolute -inset-0.5 custom-bg-gradient -z-20 rounded-[28px] group-hover:bg-gray-200" />
       )}
-      {children}
     </div>
   );
 }
@@ -42,6 +55,14 @@ SearchBoxComponent.ButtonIcon = ButtonIcon;
 
 export default SearchBoxComponent;
 
+/**
+ * A button component that toggles between a search and a clear (X) icon.
+ *
+ * @param isSearch - Determines whether to show the search icon or the clear (X) icon.
+ * @param buttonProps - Additional props passed to the button component.
+ *
+ * @returns A button with either a search or clear icon.
+ */
 function ButtonIcon(
   props: { isSearch: boolean } & ButtonHTMLAttributes<HTMLButtonElement>,
 ) {
