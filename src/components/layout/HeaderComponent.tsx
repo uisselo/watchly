dcd5d51d1f26f34c8c438ef0aa045adcc2a675cc
uisselo <louissemevale@gmail.com>
2025-01-31 19:29:28 +0800

    style(HeaderComponent): update classes with block to flex, and apply className on Link component

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -34,7 +34,11 @@ function HeaderComponent() {
             onClick={() => setIsNavModalOpen(!isNavModalOpen)}
           />
           <Link to="/">
-            <img src={SVGs.logo} alt="Watchly Logo" className="w-[120px]" />
+            <img
+              src={SVGs.logo}
+              alt="Watchly Logo"
+              className="w-[120px]"
+            />
           </Link>
         </div>
         <div className="flex items-center gap-16 md:col-span-4 lg:col-span-7">
@@ -51,13 +55,10 @@ function HeaderComponent() {
             </div>
           </div>
           <div className="flex items-center gap-3 grow">
-            <SearchBoxComponent className="flex hidden md:block lg:min-w-0 lg:w-0 grow" />
-            <ButtonComponent text="Sign up" className="hidden lg:block" />
-            <Link to="/browse">
-              <ButtonIconComponent
-                icon={MagnifyingGlassIcon}
-                className="md:hidden"
-              />
+            <SearchBoxComponent className="hidden md:flex" />
+            <ButtonComponent text="Sign up" className="hidden lg:flex" />
+            <Link to="/browse" className="md:hidden">
+              <ButtonIconComponent icon={MagnifyingGlassIcon} />
             </Link>
           </div>
         </div>
