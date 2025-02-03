49617ce6762e0140735c11d93bd59edc2c32e5d2
uisselo <louissemevale@gmail.com>
2025-02-03 21:45:14 +0800

    feat(components/layout): integrate modal from common components for responsive nav

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -1,73 +1,81 @@
-import { useState } from "react";
-import { Link } from "react-router";
+import { useEffect, useRef } from "react";
+import { Link, useLocation } from "react-router";
 import {
   Bars3Icon,
   MagnifyingGlassIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import { useMeasure } from "@uidotdev/usehooks";
 import { SVGs } from "@Assets";
 import { HEADER_NAV_ITEMS } from "@Config";
 import {
   ButtonComponent,
   ButtonIconComponent,
+  ModalComponent,
+  type ModalComponentFunctions,
   SearchBoxComponent,
 } from "../common";
 import NavItemComponent from "./NavItemComponent";
 import NavModalComponent from "./NavModalComponent";
 
-function HeaderComponent() {
-  const [ref, { height: headerHeight }] = useMeasure();
+function HeaderComponent({ onClose }: { onClose?: () => void }) {
+  const location = useLocation();
+  const navModalComponentRef = useRef<ModalComponentFunctions>(null);
 
-  const [isNavModalOpen, setIsNavModalOpen] = useState(false);
+  const handleNavClick = () => {
+    if (onClose) onClose();
+    navModalComponentRef.current?.open();
+  };
+
+  /**
+   * biome-ignore lint/correctness/useExhaustiveDependencies:
+   *  Should always run every time the route changes.
+   */
+  useEffect(() => {
+    navModalComponentRef.current?.close();
+  }, [location, navModalComponentRef]);
 
   return (
-    <>
-      <header
-        ref={ref}
-        className="container sticky top-0 z-20 flex items-center justify-between py-4 bg-gray-800 lg:grid-container lg:py-6"
-      >
-        <div className="flex items-center gap-4 md:col-span-8 lg:col-span-5">
-          <ButtonIconComponent
-            icon={isNavModalOpen ? XMarkIcon : Bars3Icon}
-            className="lg:hidden"
-            onClick={() => setIsNavModalOpen(!isNavModalOpen)}
-          />
-          <Link to="/">
-            <img
-              src={SVGs.logo}
-              alt="Watchly Logo"
-              className="w-[120px]"
-            />
+    <header className="container sticky top-0 z-20 flex items-center justify-between py-4 bg-gray-800 lg:grid-container lg:py-6">
+      <div className="flex items-center gap-4 md:col-span-8 lg:col-span-5">
+        <ButtonIconComponent
+          icon={onClose ? XMarkIcon : Bars3Icon}
+          className="lg:hidden"
+          onClick={handleNavClick}
+        />
+        <Link to="/">
+          <img src={SVGs.logo} alt="Watchly Logo" className="w-[120px]" />
+        </Link>
+      </div>
+      <div className="flex items-center gap-16 md:col-span-4 lg:col-span-7">
+        <div className="hidden lg:block">
+          <div className="flex items-center justify-end gap-12 w-max">
+            {HEADER_NAV_ITEMS.map((item) => (
+              <NavItemComponent
+                key={item.link}
+                to={item.link}
+                text={item.label}
+                isDesktop
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-3 grow">
+          <SearchBoxComponent className="hidden grow md:flex" />
+          <ButtonComponent text="Sign up" className="hidden lg:flex" />
+          <Link to="/browse" className="md:hidden">
+            <ButtonIconComponent icon={MagnifyingGlassIcon} />
           </Link>
         </div>
-        <div className="flex items-center gap-16 md:col-span-4 lg:col-span-7">
-          <div className="hidden lg:block">
-            <div className="flex items-center justify-end gap-12 w-max">
-              {HEADER_NAV_ITEMS.map((item) => (
-                <NavItemComponent
-                  key={item.link}
-                  to={item.link}
-                  text={item.label}
-                  isDesktop
-                />
-              ))}
-            </div>
-          </div>
-          <div className="flex items-center gap-3 grow">
-            <SearchBoxComponent className="hidden md:flex" />
-            <ButtonComponent text="Sign up" className="hidden lg:flex" />
-            <Link to="/browse" className="md:hidden">
-              <ButtonIconComponent icon={MagnifyingGlassIcon} />
-            </Link>
-          </div>
-        </div>
-      </header>
-      <NavModalComponent
-        isOpen={isNavModalOpen}
-        marginTop={`${headerHeight}px`}
-      />
-    </>
+      </div>
+      <ModalComponent
+        ref={navModalComponentRef}
+        className="z-50 bg-gray-800"
+      >
+        <NavModalComponent
+          onClose={() => navModalComponentRef.current?.close()}
+        />
+      </ModalComponent>
+    </header>
   );
 }
 
--- src/components/layout/NavModalComponent.tsx
+++ src/components/layout/NavModalComponent.tsx
@@ -1,79 +1,63 @@
-import { useEffect } from "react";
 import { SVGs } from "@Assets";
 import { FOOTER_NAV_ITEMS, HEADER_NAV_ITEMS } from "@Config";
 import { useBreakpoints } from "@Utilities";
 import { ButtonComponent } from "../common";
 import NavItemComponent from "./NavItemComponent";
+import HeaderComponent from "./HeaderComponent";
 
-type Props = { isOpen: boolean; marginTop: string };
-
-function NavModalComponent(props: Props) {
-  const { isOpen, marginTop } = props;
-
+function NavModalComponent({ onClose }: { onClose: () => void }) {
   const { isMediumScreen } = useBreakpoints();
-
   const links = HEADER_NAV_ITEMS.concat(FOOTER_NAV_ITEMS);
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden";
-    }
-
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, [isOpen]);
-
   return (
-    isOpen && (
-      <div className="fixed inset-0 z-50" style={{ marginTop }}>
-        <div className="container absolute inset-0 bg-gray-800 md:grid-container">
-          <div className="md:col-span-10 md:col-start-2">
-            <div className="space-y-16">
-              <div className="flex flex-col">
-                {links.map((item, index) => (
-                  <NavItemComponent
-                    key={item.link}
-                    to={item.link}
-                    text={item.label}
-                    className="animate__animated animate__fadeInLeft"
-                    style={{ animationDelay: `${index * 0.025}s` }}
-                  />
-                ))}
-              </div>
-              <div className="space-y-4 animate__animated animate__fadeInUp">
-                <p>
-                  Sign in or Register to add to your watchlist, rate
-                  movies, and write reviews!
-                </p>
-                <div className="flex gap-2">
-                  <ButtonComponent
-                    text="Sign in"
-                    variant="outline"
-                    size={isMediumScreen ? "base" : "sm"}
-                  />
-                  <ButtonComponent
-                    text="Register"
-                    size={isMediumScreen ? "base" : "sm"}
-                  />
-                </div>
-              </div>
+    <div className="space-y-8">
+      <HeaderComponent onClose={onClose} />
+      <div className="container md:grid-container">
+        <div className="md:col-span-10 md:col-start-2">
+          <div className="space-y-16">
+            <div className="flex flex-col">
+              {links.map((item, index) => (
+                <NavItemComponent
+                  key={item.link}
+                  to={item.link}
+                  text={item.label}
+                  className="animate__animated animate__fadeInLeft"
+                  style={{ animationDelay: `${index * 0.025}s` }}
+                />
+              ))}
             </div>
-            <div className="flex flex-col gap-4 py-8 animate__animated animate__fadeInUp">
-              <img
-                src={SVGs.tmdb_logo}
-                alt="TMDB Logo"
-                className="w-[100px]"
-              />
+            <div className="space-y-4 animate__animated animate__fadeInUp">
               <p>
-                Watchly uses TMDB but is not endorsed, certified, or
-                otherwise approved by TMDB.
+                Sign in or Register to add to your watchlist, rate movies,
+                and write reviews!
               </p>
+              <div className="flex gap-2">
+                <ButtonComponent
+                  text="Sign in"
+                  variant="outline"
+                  size={isMediumScreen ? "base" : "sm"}
+                />
+                <ButtonComponent
+                  text="Register"
+                  size={isMediumScreen ? "base" : "sm"}
+                />
+              </div>
             </div>
+          </div>
+          <div className="flex flex-col gap-4 py-8 animate__animated animate__fadeInUp">
+            <img
+              src={SVGs.tmdb_logo}
+              alt="TMDB Logo"
+              className="w-[100px]"
+            />
+            <p>
+              Watchly uses TMDB but is not endorsed, certified, or
+              otherwise approved by TMDB.
+            </p>
           </div>
         </div>
       </div>
-    )
+    </div>
   );
 }
 
