f580774b1cb03f4501f330433c82ce94a159f4f7
Louisse <louissevale@icloud.com>
2025-02-04 01:57:00 +0800

    feat(HeaderComponent): apply autocomplete search functionality for desktop view

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -1,5 +1,10 @@
-import { useEffect, useRef } from "react";
-import { Link, useLocation } from "react-router";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
+import {
+  Combobox,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
 import {
   Bars3Icon,
   MagnifyingGlassIcon,
@@ -7,6 +12,14 @@ import {
 } from "@heroicons/react/24/outline";
 import { SVGs } from "@Assets";
 import { HEADER_NAV_ITEMS } from "@Config";
+import {
+  type MovieItem,
+  MovieItemComponent,
+  updateDataDisplayLength,
+  useBrowseMovieQueries,
+  useBrowseMovieStore,
+} from "@Modules";
+import { useBreakpoints } from "@Utilities";
 import {
   ButtonComponent,
   ButtonIconComponent,
@@ -32,7 +45,7 @@ function HeaderComponent({ onClose }: { onClose?: () => void }) {
    */
   useEffect(() => {
     navModalComponentRef.current?.close();
-  }, [location, navModalComponentRef]);
+  }, [location]);
 
   return (
     <header className="container sticky top-0 z-20 flex items-center justify-between py-4 bg-gray-800 lg:grid-container lg:py-6">
@@ -60,11 +73,12 @@ function HeaderComponent({ onClose }: { onClose?: () => void }) {
           </div>
         </div>
         <div className="flex items-center gap-3 grow">
-          <SearchBoxComponent className="hidden grow md:flex" />
+          <AutocompleteSearch />
           <ButtonComponent text="Sign up" className="hidden lg:flex" />
-          <Link to="/browse" className="md:hidden">
-            <ButtonIconComponent icon={MagnifyingGlassIcon} />
-          </Link>
+          <ButtonIconComponent
+            icon={MagnifyingGlassIcon}
+            className="md:hidden"
+          />
         </div>
       </div>
       <ModalComponent
@@ -80,3 +94,53 @@ function HeaderComponent({ onClose }: { onClose?: () => void }) {
 }
 
 export default HeaderComponent;
+
+function AutocompleteSearch() {
+  const navigate = useNavigate();
+  const { isLargeScreen } = useBreakpoints();
+
+  const [query, setQuery] = useState("");
+
+  const { searchResults } = useBrowseMovieStore();
+  useBrowseMovieQueries(query);
+
+  const data = useMemo(
+    () => updateDataDisplayLength(searchResults, 3),
+    [searchResults],
+  );
+
+  const onChange = (item: MovieItem | null) => {
+    if (item) return navigate(`/movie/${item.id}`);
+  };
+
+  return (
+    <Combobox onChange={onChange}>
+      <div className="relative grow">
+        <SearchBoxComponent
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="hidden md:flex"
+          isAutocomplete
+        />
+        {isLargeScreen && (
+          <ComboboxOptions
+            as="div"
+            className="absolute inset-x-0 top-0 z-50 py-2 translate-y-16 bg-gray-600 rounded-lg"
+          >
+            {data.map((item) => (
+              <ComboboxOption key={item.id} value={item}>
+                <MovieItemComponent
+                  data={item}
+                  className="grid grid-cols-4 gap-3 px-3 py-2 hover:bg-gray-700"
+                >
+                  <MovieItemComponent.Image />
+                  <MovieItemComponent.Caption className="col-span-3" />
+                </MovieItemComponent>
+              </ComboboxOption>
+            ))}
+          </ComboboxOptions>
+        )}
+      </div>
+    </Combobox>
+  );
+}
